91e7697b7aa821248880d4d221839b87d03f7253
Sithuliso <okasithuli@gmail.com>
2022-11-14 12:57:30 +0200

    adding login methods

--- src/app/screens/login/login.page.ts
+++ src/app/screens/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  private user = {} as User;
+  private loading;
+  constructor(
+    private toast: ToastController,
+    private load: LoadingController,
+    private Auth: AngularFireAuth,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  showToast(messege: string) {
+    this.toast
+      .create({
+        message: messege,
+        duration: 3000,
+        position: 'top',
+      })
+      .then((toastData) => toastData.present());
   }
 
+  validation() {
+    if (!this.user.email) {
+      this.showToast('Please enter an email address');
+      return false;
+    }
+    if (!this.user.password) {
+      this.showToast('Please enter password');
+      return false;
+    }
+    return true;
+  }
+
+  login()
+  {
+    this.load.create({
+      message: 'Autheticating...',
+      spinner: 'circles',
+      duration: 50000,
+    }).then((overlay)=>
+    {
+      this.loading = overlay;
+    });
+    setTimeout(()=>
+    {
+      this.loading.dismiss();
+      if(this.validation())
+      { this.Auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+      .then(async (userCredential)=>
+      {
+        this.loading.present();
+        const user = this.Auth.currentUser;
+        if(user)
+        {
+          const emailVerified = (await user).emailVerified;
+          if(emailVerified == true)
+          {
+            const params: NavigationExtras={queryParams:{userVal:this.user.name}};
+            this.router.navigate(['home'], params);
+            localStorage.setItem('loggedinuser', this.user.email)
+            this.loading.dismiss();
+          }
+          else
+          {
+            this.showToast("please verify your email");
+          }
+        }
+      })
+      .catch((error)=>{
+        this.showToast(error);
+        this.loading.dismiss();
+      });
+      }
+    }, 2000)
+  }
 }
