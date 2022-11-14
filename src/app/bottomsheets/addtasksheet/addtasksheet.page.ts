f8ebda834fa3aa6ae46c608137025f891f43f9bd
Sithuliso <okasithuli@gmail.com>
2022-11-14 13:26:02 +0200

    adding tasks methods

--- src/app/bottomsheets/addtasksheet/addtasksheet.page.ts
+++ src/app/bottomsheets/addtasksheet/addtasksheet.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { User } from 'src/app/models/user.model';
+import { tasks } from 'src/app/models/tasks.model';
 @Component({
   selector: 'app-addtasksheet',
   templateUrl: './addtasksheet.page.html',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddtasksheetPage implements OnInit {
 
-  constructor() { }
+  tasks={} as tasks
+  user={} as User
+  loggedUser: string
+  constructor(
+    private store: AngularFirestore,
+    private toast: ToastController
+  ) {}
 
   ngOnInit() {
+    this.loggedUser = localStorage.getItem('loggedinuser');
   }
 
+  showToast(messege: string) {
+    this.toast
+      .create({
+        message: messege,
+        duration: 3000,
+        position: 'top',
+      })
+      .then((toastData) => toastData.present());
+  }
+
+ async addtasks()
+  {
+    this.store.collection('tasks').doc(this.loggedUser).set({
+      title: this.tasks.title,
+      notes: this.tasks.notes,
+      deadline: this.tasks.deadline
+    });
+  }
 }
---
+++ src/app/models/tasks.model.ts
@@ -0,0 +1,5 @@
+export interface tasks{
+    title: string,
+    notes: string;
+    deadline: string
+}
