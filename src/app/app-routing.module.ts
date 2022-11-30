7e502b24a46e67af80d9c15130223b9fb0d5845c
Sithuliso <okasithuli@gmail.com>
2022-11-30 09:04:58 +0200

    adding splash splash screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,60 +5,96 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-   import('./screens/login/login.module').then( m => m.LoginPageModule)
+      import('./screens/splash/splash.module').then((m) => m.SplashPageModule),
   },
   {
     path: 'home',
-    loadChildren: () => import('./screens/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./screens/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: 'tasks',
-    loadChildren: () => import('./screens/tasks/tasks.module').then( m => m.TasksPageModule)
+    loadChildren: () =>
+      import('./screens/tasks/tasks.module').then((m) => m.TasksPageModule),
   },
   {
     path: 'add-task',
-    loadChildren: () => import('./screens/add-task/add-task.module').then( m => m.AddTaskPageModule)
+    loadChildren: () =>
+      import('./screens/add-task/add-task.module').then(
+        (m) => m.AddTaskPageModule
+      ),
   },
   {
     path: 'liked-tasks',
-    loadChildren: () => import('./screens/liked-tasks/liked-tasks.module').then( m => m.LikedTasksPageModule)
+    loadChildren: () =>
+      import('./screens/liked-tasks/liked-tasks.module').then(
+        (m) => m.LikedTasksPageModule
+      ),
   },
   {
     path: 'account',
-    loadChildren: () => import('./screens/account/account.module').then( m => m.AccountPageModule)
+    loadChildren: () =>
+      import('./screens/account/account.module').then(
+        (m) => m.AccountPageModule
+      ),
   },
   {
     path: 'login',
-    loadChildren: () => import('./screens/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./screens/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'signup',
-    loadChildren: () => import('./screens/signup/signup.module').then( m => m.SignupPageModule)
+    loadChildren: () =>
+      import('./screens/signup/signup.module').then((m) => m.SignupPageModule),
   },
   {
     path: 'addtasksheet',
-    loadChildren: () => import('./bottomsheets/addtasksheet/addtasksheet.module').then( m => m.AddtasksheetPageModule)
+    loadChildren: () =>
+      import('./bottomsheets/addtasksheet/addtasksheet.module').then(
+        (m) => m.AddtasksheetPageModule
+      ),
   },
   {
     path: 'taskdetails',
-    loadChildren: () => import('./screens/taskdetails/taskdetails.module').then( m => m.TaskdetailsPageModule)
+    loadChildren: () =>
+      import('./screens/taskdetails/taskdetails.module').then(
+        (m) => m.TaskdetailsPageModule
+      ),
   },
   {
     path: 'logout',
-    loadChildren: () => import('./bottomsheets/logout/logout.module').then( m => m.LogoutPageModule)
+    loadChildren: () =>
+      import('./bottomsheets/logout/logout.module').then(
+        (m) => m.LogoutPageModule
+      ),
   },
   {
     path: 'delete-account',
-    loadChildren: () => import('./bottomsheets/delete-account/delete-account.module').then( m => m.DeleteAccountPageModule)
-  },  {
+    loadChildren: () =>
+      import('./bottomsheets/delete-account/delete-account.module').then(
+        (m) => m.DeleteAccountPageModule
+      ),
+  },
+  {
     path: 'notasks',
-    loadChildren: () => import('./bottomsheets/notasks/notasks.module').then( m => m.NotasksPageModule)
+    loadChildren: () =>
+      import('./bottomsheets/notasks/notasks.module').then(
+        (m) => m.NotasksPageModule
+      ),
   },
   {
     path: 'alltasks',
-    loadChildren: () => import('./screens/alltasks/alltasks.module').then( m => m.AlltasksPageModule)
-  }
-
+    loadChildren: () =>
+      import('./screens/alltasks/alltasks.module').then(
+        (m) => m.AlltasksPageModule
+      ),
+  },
+  {
+    path: 'splash',
+    loadChildren: () =>
+      import('./screens/splash/splash.module').then((m) => m.SplashPageModule),
+  },
 ];
 @NgModule({
   imports: [
---
+++ src/app/screens/splash/splash.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StatusBar } from '@capacitor/status-bar';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+})
+export class SplashPage implements OnInit {
+
+  constructor(private router:Router) {
+    setTimeout(() =>
+    {
+      this.router.navigateByUrl('login');
+    },4000)
+   }
+
+  ngOnInit() {
+    StatusBar.setBackgroundColor({ color: '#393E46'});
+  }
+
+}
